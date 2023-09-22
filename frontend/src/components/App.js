46e8c7dd64d384d9ecc0f8181410435b4d5b7b07
roylee0912 <leeroy0912@gmail.com>
2023-09-21 17:45:26 -0700

    redesign

---
+++ frontend/src/components/App.js
@@ -0,0 +1,17 @@
+import {useState, useEffect} from "react"
+import '../styles/App.css';
+
+
+function App() {
+
+
+
+  return (
+    <>
+    <div className="App">
+    </div>
+    </>
+  );
+}
+
+export default App;
