9c36a5a35c59b46dae1de06dc91cf1723fe49b44
roylee0912 <leeroy0912@gmail.com>
2023-10-19 23:21:22 -0700

    updated celery task; just need to dockerize and deploy

--- back/Scraper/views.py
+++ back/Scraper/views.py
@@ -19,8 +19,11 @@ def get_job_listings(request):
 
         return Response(serializer.data, status=status.HTTP_200_OK, content_type='application/json')
 
-    elif (request.method == 'POST'):
-        JobListings.objects.all().delete()
+
+
+
+@shared_task
+def scrape_and_create_job():
         word_categories={
         "intern": "Internship",
         "co-op": "Co-op",
@@ -244,134 +247,4 @@ def get_job_listings(request):
 
 
 
-
-
-
-
-@shared_task
-def scrape_and_create_job():
-    word_categories={
-    "intern": "Internship",
-    "co-op": "Co-op",
-    "machine learning": "Machine Learning",
-    "ml": "Machine Learning",
-    "artifical intelligence": "Machine Learning",
-    "software": "Software Engineering",
-    "data": "Data Science",
-    "research": "Research",
-    }
-
-    page = requests.get("https://github.com/SimplifyJobs/Summer2024-Internships")
-    soup = BeautifulSoup(page.content, 'html.parser')
-    job_listings = []
-    # List to keep track of failed creations
-    failed_creations = []
-    
-    for row in soup.find_all('tr'):
-        cols = row.find_all('td')
-        
-        # Ensure there are at least 5 columns (Company, Job Title, Location, Apply Link, Date)
-        if len(cols) < 5:
-            continue
-        
-        # Extract Company Name
-        company = cols[0].get_text(strip=True)
-
-        # If the company name is a link, extract the link too
-        company_link = cols[0].find('a')
-        final_company_link=''
-        if company_link:
-            final_company_link = company_link['href']
-        
-        # Extract Job Title, Location, Date
-        company_requires_sponsorship=False
-
-        #check if requires sponsorship
-        job_title = cols[1].get_text(strip=True)
-
-        if "🛂" in job_title:
-            # this line of code isn't working for some reason. It's not reupdating the value of the company_requires_sponsorship variable.
-            job_title=job_title.replace("🛂","")
-            company_requires_sponsorship=True
-
-
-        #check categories this belongs to
-        categories=[]
-        for key in word_categories:
-            if key in job_title.lower():
-                categories.append(word_categories[key])
-                
-
-        
-        #check if already closed
-        is_closed=False
-        if "🔒" in cols[3].get_text(strip=True):
-            is_closed=True
-        location = cols[2].get_text(strip=True)
-        date = cols[4].get_text(strip=True)
-
-        
-        # Extract Apply Link and icons
-        apply_link_tag = cols[3].find('a', rel='nofollow')
-        if apply_link_tag:
-            apply_link = apply_link_tag['href']
-        else:
-            apply_link = None
-            is_closed=True #should've been accounted for earlier, but just in case it's not already done, then check if closed.
-
-
-        #final error handling: if company name is empty, then add company name and link from the last available one
-        if "↳" in company:
-            i=-1
-            while ("↳" in job_listings[-1]["company"]):
-                i-=1
-            company=job_listings[i]["company"]
-            company_link=job_listings[i]["company_link"]
-
-        job={
-            "company": company,
-            "company_link":final_company_link,
-            "title": job_title,
-            "location": location,
-            "apply_link": apply_link,
-            "date": date,
-            "closed": is_closed,
-            "sponsorship": company_requires_sponsorship,
-            "categories": categories
-        }
-        job_listings.append(job)
-
-        existing_job = JobListings.objects.filter(apply_link=job['apply_link']).first()
-        if not existing_job:
-            job_serializer = JobListingSerializer(data=job)
-            if job_serializer.is_valid():
-                job_serializer.save()
-            else:
-                # If serialization failed, store the job details for debugging purposes
-                failed_job = job
-                failed_job["errors"] = job_serializer.errors
-                failed_creations.append(failed_job)
-        else:
-            # the job already exists. 
-            updated = False  # flag to check if an update is needed
-            
-            # check if there is any change in the application status
-            if is_closed != existing_job.closed:
-                existing_job.closed = is_closed
-                updated = True
-                
-            # check if there is any change in the sponsorship status and change
-            if company_requires_sponsorship != existing_job.sponsorship:
-                existing_job.sponsorship = company_requires_sponsorship
-                updated = True
-
-            # Save only once if any updates were made
-            if updated:
-                existing_job.save()
-
-    # Return a JsonResponse with the number of successful and failed creations
-    if not failed_creations:
-        return JsonResponse({"message": "All job listings created successfully."}, status=status.HTTP_201_CREATED)
-    else:
-        return JsonResponse({"message": f"{len(failed_creations)} job listings failed to be created.", "details": failed_creations}, status=status.HTTP_400_BAD_REQUEST)
 
