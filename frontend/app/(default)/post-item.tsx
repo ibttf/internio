fb2448bbf620414ae05dae189ae35d5dcddc53f2
roylee0912 <leeroy0912@gmail.com>
2023-10-19 22:12:34 -0700

    styling changes on frontend; fixed the location glitchiness just having it as a string

--- frontend/app/(default)/post-item.tsx
+++ frontend/app/(default)/post-item.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { FaLock } from "react-icons/fa";
+import { FaLock, FaCheckCircle } from "react-icons/fa";
 export default function PostItem({ ...props }) {
   const getCategoryColor = (category: string) => {
     switch (category) {
@@ -34,102 +34,107 @@ export default function PostItem({ ...props }) {
     const randomIndex = Math.floor(Math.random() * tailwindColors.length);
     return tailwindColors[randomIndex];
   };
-
   return (
-    <div
-      className={`[&:nth-child(-n+12)]:-order-1 group ${
-        !props.sticky && "border-b border-gray-200"
-      }`}
-    >
-      <div className={`px-4 py-6 ${"bg-indigo-100 rounded-xl"}`}>
-        <div className="sm:flex items-center space-y-3 sm:space-y-0 sm:space-x-5">
-          <div className="shrink-0">
+    <div className={`group border-b ${!props.sticky && "border-gray-200"}`}>
+      <div className="px-6 py-6 bg-indigo-100 hover:bg-indigo-200 transition duration-300 ease-in-out rounded-lg shadow-md relative">
+        {/* Date in top right corner */}
+        <div className="absolute top-3 right-3 text-sm text-gray-400">
+          Posted: {props.date}
+        </div>
+
+        <div className="flex items-center space-x-5">
+          <div className="flex-shrink-0">
             {props.image ? (
               <img
                 src={props.image}
-                width="56"
-                height="56"
+                width="64"
+                height="64"
                 alt={props.company}
+                className="rounded-full border border-gray-200 shadow-sm"
               />
             ) : (
               <div
-                className={`w-14 h-14 flex items-center justify-center text-white text-xl font-bold rounded-full ${getRandomColor()}`}
+                className={`w-16 h-16 flex items-center justify-center text-white text-xl font-bold rounded-full ${getRandomColor()}`}
               >
                 {props.company[0]}
               </div>
             )}
           </div>
-          <div className="grow lg:flex items-center justify-between space-y-5 lg:space-x-2 lg:space-y-0">
-            <div>
-              <div className="flex datas-start space-x-2">
-                <div className="text-sm text-gray-800 font-semibold mb-1">
-                  {props.title}
+          <div className="flex-grow space-y-4">
+            {props.company_link ? (
+              <Link
+                className="text-xl font-semibold text-blue-600 hover:underline"
+                href={`${props.company_link}`}
+              >
+                {props.company}
+              </Link>
+            ) : (
+              <div className="text-xl font-semibold text-gray-800">
+                {props.company}
+              </div>
+            )}
+            <div className="flex items-center space-x-2">
+              <div className="text-gray-500">{props.title}</div>
+            </div>
+            <div className="flex flex-wrap items-center space-x-2 mt-2 space-y-2">
+              {/* Location as a colorful box */}
+              {props.locations ? (
+                props.locations.map((location: string, index: number) => (
+                  <span
+                    key={index}
+                    className={`text-xs font-semibold px-2 py-0.5 rounded-full ${
+                      location === "Remote"
+                        ? "bg-gray-800 text-gray-50"
+                        : "bg-blue-200 text-blue-700"
+                    }`}
+                  >
+                    {location}
+                  </span>
+                ))
+              ) : (
+                <></>
+              )}
+              {props.categories.map((category: string, index: number) => (
+                <span
+                  key={index}
+                  className={`text-xs font-semibold px-2 py-0.5 bg-gray-100 rounded-full ${getCategoryColor(
+                    category
+                  )}`}
+                >
+                  {category}
+                </span>
+              ))}
+            </div>
+            <div className="flex items-center space-x-2 mt-2 opacity-70">
+              {/* Sponsorship */}
+              {props.sponsorship ? (
+                <div className="flex items-center text-sm text-green-600">
+                  <FaCheckCircle size={16} className="mr-2" />
+                  <span>Doesn't require sponsorship</span>
                 </div>
-              </div>
-              <div className="mb-2">
-                {props.company_link ? (
-                  <Link
-                    className="text-lg font-bold text-blue-600 underline hover:cursor-pointer"
-                    href={`${props.company_link}`}
-                  >
-                    {props.company}
-                  </Link>
-                ) : (
-                  <div className="text-lg text-gray-800 font-bold">
-                    {props.company}
-                  </div>
-                )}
-              </div>
-              <div className="-m-1 flex flex-wrap">
-                {props.categories.forEach((category: any) => {
-                  console.log(category);
-                })}
-                {props.categories.map((category: string, index: number) => {
-                  return (
-                    <span
-                      key={index}
-                      className={`text-xs font-medium inline-flex px-2 py-0.5 rounded-md m-1 whitespace-nowrap transition duration-150 ease-in-out ${
-                        props.sticky
-                          ? "bg-indigo-50"
-                          : getCategoryColor(category)
-                      }`}
-                    >
-                      {category}
-                    </span>
-                  );
-                })}
-              </div>
-              <div>
-                {props.sponsorship ? (
-                  <div>requires sponsorship</div>
-                ) : (
-                  <div> doesn't require sponsorship</div>
-                )}
-              </div>
+              ) : (
+                <div className="flex items-center text-sm text-red-600">
+                  <FaLock size={16} className="mr-2" />
+                  <span>Requires sponsorship</span>
+                </div>
+              )}
             </div>
-            <div className="min-w-[120px] flex items-center lg:justify-end space-x-3 lg:space-x-0">
-              <div className="">
-                {props.closed ? (
-                  <div className="flex items-center text-white space-x-1 bg-yellow-600 py-1.5 px-3 rounded-full btn-sm">
-                    <FaLock size={12} />
-                    <span>Closed</span>
-                  </div>
-                ) : (
-                  <Link
-                    className="btn-sm py-1.5 px-3 text-white bg-indigo-500 hover:bg-indigo-600 group shadow-sm"
-                    href={`${props.apply_link}`}
-                  >
-                    Apply
-                    <span className="tracking-normal text-indigo-200 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">
-                      -&gt;
-                    </span>
-                  </Link>
-                )}
+          </div>
+
+          <div className="flex-shrink-0">
+            {props.closed ? (
+              <div className="flex items-center text-white bg-yellow-600 px-3 py-1.5 rounded-full">
+                <FaLock size={12} className="mr-2" />
+                <span>Closed</span>
               </div>
-              {/* <div className="group-hover:lg:hidden text-sm italic text-gray-500">
-                {props.date}
-              </div> */}
-            </div>
+            ) : (
+              <Link
+                className="text-white bg-indigo-500 hover:bg-indigo-600 px-3 py-1.5 rounded-full transition duration-150"
+                href={`${props.apply_link}`}
+              >
+                Apply
+              </Link>
+            )}
           </div>
         </div>
       </div>
--- frontend/app/(default)/posts-list.tsx
+++ frontend/app/(default)/posts-list.tsx
@@ -8,7 +8,7 @@ interface Post {
   title: string;
   company: string;
   company_link: string;
-  location: string;
+  locations: string[];
   apply_link: string;
   sponsorship: boolean;
   closed: boolean;
@@ -17,6 +17,7 @@ interface Post {
 
 export default function PostsList() {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
   useEffect(() => {
     async function fetchPosts() {
@@ -28,12 +29,35 @@ export default function PostsList() {
     fetchPosts();
   }, []);
 
+  // Filter the posts based on the search query
+  const filteredPosts = posts.filter((post: Post) => {
+    const searchLower = searchQuery.toLowerCase();
+    return (
+      post.title.toLowerCase().includes(searchLower) ||
+      post.company.toLowerCase().includes(searchLower) ||
+      post.categories.some((category) =>
+        category.toLowerCase().includes(searchLower)
+      ) ||
+      post.locations.some((location) =>
+        location.toLowerCase().includes(searchLower)
+      )
+    );
+  });
   return (
     <div className="pb-8 md:pb-16">
       <h2 className="text-3xl font-bold font-inter mb-10">Latest jobs</h2>
+      {/* Search bar */}
+      <input
+        type="text"
+        placeholder="Search jobs..."
+        className="bg-gray-200 border-2 border-blue-300 rounded-full py-2 px-4 text-gray-700 focus:outline-none focus:border-blue-500 placeholder-gray-500 w-full max-w-md mx-auto"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+
       {/* List container */}
-      <div className="flex flex-col">
-        {posts.map((post: Post) => {
+      <div className="flex flex-col space-y-2">
+        {filteredPosts.map((post: Post) => {
           return <PostItem key={post.id} {...post} />;
         })}
       </div>
