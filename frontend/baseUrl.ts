ef4879ea2695310804971ea42d4997ac79df65bb
roylee0912 <leeroy0912@gmail.com>
2023-11-01 20:48:51 -0700

    changed base url to actual base url

--- frontend/baseUrl.ts
+++ frontend/baseUrl.ts
@@ -1,3 +1,4 @@
-const config = { baseUrl: "https://3.135.236.197" };
+const config = { baseUrl: "https://internio-backend.com" };
+
 // const config = { baseUrl: "http://localhost:8000" };
 export default config;
